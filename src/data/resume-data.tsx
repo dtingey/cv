06c38a7407e0599a374d27827dee602f13e5e549
Damon Tingey <damontingey@Damons-MBP.lan>
2023-12-26 20:20:02 -0700

    update data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -27,7 +27,7 @@ export const RESUME_DATA = {
   about:
     "Full Stack Data Scientist focused on building data products that solve real-world problems.",
   summary:
-    "As a Data Scientist, I have experience in building data pipelines, training and deploying machine learning models, and building data visualizations. I work mostly with Python, PyTorch, Plotly Dash.I have 2 years of experience in working with real-world data to solve real-world problems.",
+    "As a Data Scientist, I have experience in building data pipelines, training and deploying machine learning models, and building data visualizations. I work mostly with Python, PyTorch, Plotly Dash, and . I have 2 years of experience in working with real-world data to solve real-world problems.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
   personalWebsiteUrl: "https://damontingey.vercel.app/",
   contact: {
@@ -72,74 +72,63 @@ export const RESUME_DATA = {
         "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: Plotly Dash, Python, JavaScript, MongoDB, Airflow, Kubernetes, AWS",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "Brigham Young University - Department of Physics and Astronomy",
+      link: "https://physics.byu.edu",
+      badges: [],
+      title: "Undergraduate Research Assistant",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "2021",
+      end: "2023",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Did research in deep-learning based computational molecular dynamics simluations and computational drug discoveryTechnologies: Android, Kotlin, React, TypeScript, GraphQL",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
+      company: "The Coding School",
+      link: "https://the-cs.org",
+      badges: ['Remote'],
+      title: "Teaching Assistant",
       logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      start: "2022",
+      end: "2023",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Taught two classes of high school students the basics of python and machine learning, including deep learning and reinforcement learning.",
     },
   ],
   skills: [
     "Python",
     "PyTorch",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Plotly Dash",
+    "C/C++",
+    "Julia",
+    "Dutch",
+    "MD simulation",
+    "Quantum ML",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "INSyT - LLM Network Instrusion Detection",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Capstone Project",
+        "PyTorch",
+        "Flask",
+        "Redis",
       ],
-      description: "A platform to build and grow your online business",
+      description: "Large Language Model based network intrusion detection system",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "github.com",
+        href: "https://github.com/Isaacwilliam4/Network_Intrusion",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "TrIP",
+      techStack: ["Research Project", "PyTorch", "Docker",],
       description:
-        "Browser extension that records everything happening in a web application",
+        "Transformer based model for prediction molecular potential energy and forces",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "github.com",
+        href: "https://github.com/dellacortelab/trip",
       },
     },
     {
@@ -163,116 +152,6 @@ export const RESUME_DATA = {
         label: "useminimal.com",
         href: "https://useminimal.com/",
       },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
